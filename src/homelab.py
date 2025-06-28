a54549ed2f4f2453face3acd952eb565c46206a8
Guille <guillerosello@gmail.com>
2025-06-28 19:27:49 -0300

    Multiple tweaks and first draft

--- src/homelab.py
+++ src/homelab.py
@@ -1,8 +1,30 @@
 # diagram.py
-from diagrams import Diagram
+from diagrams import Diagram, Cluster
+from diagrams.onprem.proxmox import Pve
+from diagrams.generic.network import Firewall, Switch, Subnet
+from diagrams.custom import Custom
+from diagrams.generic.os import Ubuntu
+from diagrams.onprem.container import Docker
+from diagrams.onprem.network import Traefik
+
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+graph_attr = {
+    "fontsize": "45",
+    "bgcolor": "transparent"
+}
+
+with Diagram("Network", show=False, graph_attr=graph_attr ):
+    wan1 = Custom("WAN1", "img/wan.png")
+    wan2 = Custom("WAN2", "img/wan.png")
+    mikrotik = Custom("Mikrotik","img/mikrotik.png")
+    [wan1,wan2] >> mikrotik >> Switch("TP-Link Switch 2.5G") >> Pve("Proxmox-Hypervisor")
+
+with Diagram("Apps", show=False, graph_attr=graph_attr):
+    with Cluster("Proxmox"):
+         truenas = Custom("Truenas Scale", "img/truenas.png")
+         Ubuntu("Docker Host 1") >> [Traefik("Traefik-1"), Docker("Pihole"), Docker("Immich"), Docker("OpenSpeedTest")]
+         Ubuntu("Docker Host 2") >> [Traefik("Traefik-2"), Docker("Unifi Controller")]
+         truenas
