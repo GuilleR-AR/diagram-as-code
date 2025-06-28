356473f4cdaecfd0d238dafaa601e7d9e880fc8b
Guille <guillerosello@gmail.com>
2025-06-28 19:42:31 -0300

    added subnet

--- src/homelab.py
+++ src/homelab.py
@@ -19,8 +19,9 @@ graph_attr = {
 with Diagram("Network", show=False, graph_attr=graph_attr ):
     wan1 = Custom("WAN1", "img/wan.png")
     wan2 = Custom("WAN2", "img/wan.png")
-    mikrotik = Custom("Mikrotik","img/mikrotik.png")
-    [wan1,wan2] >> mikrotik >> Switch("TP-Link Switch 2.5G") >> Pve("Proxmox-Hypervisor")
+    with Cluster("10.2.50.1/24"):    
+        mikrotik = Custom("Mikrotik","img/mikrotik.png")
+        [wan1,wan2] >> mikrotik >> Switch("TP-Link Switch 2.5G") >> Pve("Proxmox-Hypervisor")
 
 with Diagram("Apps", show=False, graph_attr=graph_attr):
     with Cluster("Proxmox"):
